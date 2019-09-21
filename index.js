2203261c166b74ac406f915ccf30f5f717ceb647
Mark Kauffman <markkauffman22@gmail.com>
2019-09-21 21:00:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ function currentLine(line) {
      
      for (var i = 0; i < line.length; i++) {
        var pos = i + 1;
+       if (pos === 1) {
+         
+       }
        outputStr = outputStr + pos + ".  " + line[i] + " " ;
        
        //expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
